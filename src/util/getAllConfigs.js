447137ae1d39516e7e068e6cf8e3813ed4229b5b
Adam Wathan <adam.wathan@gmail.com>
2020-10-18 15:28:10 -0400

    Revert "Extend spacing scale"
    This reverts commit 5046367597de88be6d14efc96f0dc15f19ce1c52.

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -1,5 +1,6 @@
 import defaultConfig from '../../stubs/defaultConfig.stub.js'
 import { flagEnabled } from '../featureFlags'
+import extendedSpacingScale from '../flagged/extendedSpacingScale.js'
 import extendedFontSizeScale from '../flagged/extendedFontSizeScale.js'
 import additionalBreakpoint from '../flagged/additionalBreakpoint'
 import { flatMap, get } from 'lodash'
@@ -10,6 +11,7 @@ export default function getAllConfigs(config, defaultPresets = [defaultConfig]) 
   })
 
   const features = {
+    extendedSpacingScale,
     extendedFontSizeScale,
     additionalBreakpoint,
   }
