67a465b74fb5fc5946500571b8a2d2baa5a9a650
Adam Wathan <adam.wathan@gmail.com>
2020-10-18 15:28:10 -0400

    Revert "Add line-heights to font-size utilities by default"
    This reverts commit 3b56e62ce676f10a062965aa8e38d00f233270cd.

--- src/util/getAllConfigs.js
+++ src/util/getAllConfigs.js
@@ -2,6 +2,7 @@ import defaultConfig from '../../stubs/defaultConfig.stub.js'
 import { flagEnabled } from '../featureFlags'
 import uniformColorPalette from '../flagged/uniformColorPalette.js'
 import extendedSpacingScale from '../flagged/extendedSpacingScale.js'
+import defaultLineHeights from '../flagged/defaultLineHeights.js'
 import extendedFontSizeScale from '../flagged/extendedFontSizeScale.js'
 import darkModeVariant from '../flagged/darkModeVariant.js'
 import standardFontWeights from '../flagged/standardFontWeights'
@@ -16,6 +17,7 @@ export default function getAllConfigs(config, defaultPresets = [defaultConfig]) 
   const features = {
     uniformColorPalette,
     extendedSpacingScale,
+    defaultLineHeights,
     extendedFontSizeScale,
     standardFontWeights,
     darkModeVariant,
